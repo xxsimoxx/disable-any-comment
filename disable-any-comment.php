a98192915382184499fa2a4e1191203cde34c0e6
xxsimoxx <simospa@gieffeedizioni.it>
2019-12-15 15:15:27 +0100

    code style

--- disable-any-comment.php
+++ disable-any-comment.php
@@ -16,86 +16,86 @@
  */
 namespace XXSimoXX\DisableAnyComment;
 
-if( !defined( 'ABSPATH' ) )
+if(!defined('ABSPATH'))
 	exit;
 
 class DisableAnyComment {
 
 	private $networkactive;
 	
-	private $modified_types = array();
+	private $modified_types = [];
 
 	function __construct() {
-		$this->networkactive = ( is_multisite() && array_key_exists( plugin_basename( __FILE__ ), (array) get_site_option( 'active_sitewide_plugins' ) ) );
+		$this->networkactive = (is_multisite() && array_key_exists(plugin_basename(__FILE__), (array) get_site_option('active_sitewide_plugins')));
 		$this->init_filters();
 	}
 
 	private function init_filters() {
 		// These need to happen now
-		add_filter( 'wp_headers', array( $this, 'filter_wp_headers' ) );
-		add_action( 'template_redirect', array( $this, 'filter_query' ), 9 );
+		add_filter('wp_headers', [$this, 'filter_wp_headers']);
+		add_action('template_redirect', [$this, 'filter_query'], 9);
 
 		// Admin bar filtering has to happen here since WP 3.6
-		add_action( 'template_redirect', array( $this, 'filter_admin_bar' ) );
-		add_action( 'admin_init', array( $this, 'filter_admin_bar' ) );
+		add_action('template_redirect', [$this, 'filter_admin_bar']);
+		add_action('admin_init', [$this, 'filter_admin_bar']);
 
 		// These can happen later
-		add_action( 'plugins_loaded', array( $this, 'register_text_domain' ) ); 
-		add_action( 'wp_loaded', array( $this, 'init_wploaded_filters' ) );
+		add_action('plugins_loaded',[$this, 'register_text_domain']); 
+		add_action('wp_loaded', [$this, 'init_wploaded_filters']);
 	}
 
 	public function register_text_domain() {
-		load_plugin_textdomain( 'disable-comments', false, dirname( plugin_basename( __FILE__ ) ) .  '/languages' );
+		load_plugin_textdomain('disable-comments', false, dirname(plugin_basename(__FILE__)) . '/languages');
 	}
 
-	public function init_wploaded_filters(){
+	public function init_wploaded_filters() {
 	
-		$typeargs = array( 'public' => true );
-		if( $this->networkactive ) {
-			$typeargs['_builtin'] = true;	// stick to known types for network
+		$typeargs = ['public' => true];
+		if($this->networkactive) {
+			$typeargs['_builtin'] = true;
 		}
-		$disable_post_types = get_post_types( $typeargs);
-		foreach( array_keys( $disable_post_types ) as $type ) {
-			if( ! in_array( $type, $this->modified_types ) && ! post_type_supports( $type, 'comments' ) )	// the type doesn't support comments anyway
-				unset( $disable_post_types[$type] );
+		$disable_post_types = get_post_types($typeargs);
+		foreach(array_keys($disable_post_types) as $type) {
+			if(!in_array($type, $this->modified_types) && !post_type_supports($type, 'comments'))
+				unset($disable_post_types[$type]);
 		}
 
-		if( !empty( $disable_post_types ) ) {
-			foreach( $disable_post_types as $type ) {
+		if(!empty($disable_post_types)) {
+			foreach($disable_post_types as $type) {
 				// we need to know what native support was for later
-				if( post_type_supports( $type, 'comments' ) ) {
+				if(post_type_supports($type, 'comments')) {
 					$this->modified_types[] = $type;
-					remove_post_type_support( $type, 'comments' );
-					remove_post_type_support( $type, 'trackbacks' );
+					remove_post_type_support($type, 'comments');
+					remove_post_type_support($type, 'trackbacks');
 				}
 			}
-			add_filter( 'comments_array', array( $this, 'filter_existing_comments' ), 20, 2 );
-			add_filter( 'comments_open', array( $this, 'filter_comment_status' ), 20, 2 );
-			add_filter( 'pings_open', array( $this, 'filter_comment_status' ), 20, 2 );
+			add_filter('comments_array', [$this, 'filter_existing_comments'], 20, 2);
+			add_filter('comments_open', [$this, 'filter_comment_status'], 20, 2);
+			add_filter('pings_open', [$this, 'filter_comment_status'], 20, 2);
 		}
 
 		// Filters for the admin only
-		if( is_admin() ) {
-			if( $this->networkactive ) {
-				add_action( 'network_admin_menu', array( $this, 'tools_menu' ) );
-				add_filter( 'network_admin_plugin_action_links', array( $this, 'plugin_actions_links'), 10, 2 );
+		if(is_admin()) {
+			if($this->networkactive) {
+				add_action('network_admin_menu', [$this, 'tools_menu']);
+				add_filter('network_admin_plugin_action_links', [$this, 'plugin_actions_links'], 10, 2);
 			}
 			else {
-				add_action( 'admin_menu', array( $this, 'tools_menu' ) );
-				add_filter( 'plugin_action_links', array( $this, 'plugin_actions_links'), 10, 2 );
+				add_action('admin_menu', [$this, 'tools_menu' ]);
+				add_filter('plugin_action_links', [$this, 'plugin_actions_links'], 10, 2);
 			}
 
-			add_action( 'admin_menu', array( $this, 'filter_admin_menu' ), PHP_INT_MAX );
-			add_action( 'admin_print_styles-index.php', array( $this, 'admin_css' ) );
-			add_action( 'admin_print_styles-profile.php', array( $this, 'admin_css' ) );
-			add_action( 'wp_dashboard_setup', array( $this, 'filter_dashboard' ) );
-			add_filter( 'pre_option_default_pingback_flag', '__return_zero' );
+			add_action('admin_menu', [$this, 'filter_admin_menu'], PHP_INT_MAX);
+			add_action('admin_print_styles-index.php', [$this, 'admin_css']);
+			add_action('admin_print_styles-profile.php', [$this, 'admin_css']);
+			add_action('wp_dashboard_setup', [$this, 'filter_dashboard']);
+			add_filter('pre_option_default_pingback_flag', '__return_zero');
 		}
 		
 		// Filters for front end only
 		else {
-			add_action( 'template_redirect', array( $this, 'check_comment_template' ) );
-			add_filter( 'feed_links_show_comments_feed', '__return_false' );
+			add_action('template_redirect', [$this, 'check_comment_template']);
+			add_filter('feed_links_show_comments_feed', '__return_false');
 		}
 	}
 
@@ -105,28 +105,28 @@ class DisableAnyComment {
 	 * and set it to True
 	 */
 	public function check_comment_template() {
-		if( is_singular() ) {
-			if( !defined( 'DISABLE_COMMENTS_REMOVE_COMMENTS_TEMPLATE' ) || DISABLE_COMMENTS_REMOVE_COMMENTS_TEMPLATE == true ) {
+		if(is_singular()) {
+			if(!defined('DISABLE_COMMENTS_REMOVE_COMMENTS_TEMPLATE') || DISABLE_COMMENTS_REMOVE_COMMENTS_TEMPLATE == true) {
 				// Kill the comments template.
-				add_filter( 'comments_template', array( $this, 'dummy_comments_template' ), 20 );
+				add_filter('comments_template', [$this, 'dummy_comments_template'], 20);
 			}
 			// Remove comment-reply script for themes that include it indiscriminately
-			wp_deregister_script( 'comment-reply' );
+			wp_deregister_script('comment-reply');
 			// feed_links_extra inserts a comments RSS link
-			remove_action( 'wp_head', 'feed_links_extra', 3 );
+			remove_action('wp_head', 'feed_links_extra', 3);
 		}
 	}
 
 	public function dummy_comments_template() {
-		return dirname( __FILE__ ) . '/includes/comments-template.php';
+		return dirname(__FILE__) . '/includes/comments-template.php';
 	}
 
 
 	/*
 	 * Remove the X-Pingback HTTP header
 	 */
-	public function filter_wp_headers( $headers ) {
-		unset( $headers['X-Pingback'] );
+	public function filter_wp_headers($headers) {
+		unset($headers['X-Pingback']);
 		return $headers;
 	}
 
@@ -134,8 +134,8 @@ class DisableAnyComment {
 	 * Issue a 403 for all comment feed requests.
 	 */
 	public function filter_query() {
-		if( is_comment_feed() ) {
-			wp_die( __( 'Comments are closed.' ), '', array( 'response' => 403 ) );
+		if(is_comment_feed()) {
+			wp_die(__('Comments are closed.'), '', ['response' => 403]);
 		}
 	}
 
@@ -143,11 +143,11 @@ class DisableAnyComment {
 	 * Remove comment links from the admin bar.
 	 */
 	public function filter_admin_bar() {
-		if( is_admin_bar_showing() ) {
+		if(is_admin_bar_showing()) {
 			// Remove comments links from admin bar
-			remove_action( 'admin_bar_menu', 'wp_admin_bar_comments_menu', 60 );
-			if( is_multisite() ) {
-				add_action( 'admin_bar_menu', array( $this, 'remove_network_comment_links' ), 500 );
+			remove_action('admin_bar_menu', 'wp_admin_bar_comments_menu', 60);
+			if(is_multisite()) {
+				add_action('admin_bar_menu', [$this, 'remove_network_comment_links'], 500);
 			}
 		}
 	}
@@ -155,15 +155,15 @@ class DisableAnyComment {
 	/*
 	 * Remove comment links from the admin bar in a multisite network.
 	 */
-	public function remove_network_comment_links( $wp_admin_bar ) {
-		if( $this->networkactive && is_user_logged_in() ) {
-			foreach( (array) $wp_admin_bar->user->blogs as $blog ) {
-				$wp_admin_bar->remove_menu( 'blog-' . $blog->userblog_id . '-c' );
+	public function remove_network_comment_links($wp_admin_bar) {
+		if($this->networkactive && is_user_logged_in()) {
+			foreach((array) $wp_admin_bar->user->blogs as $blog) {
+				$wp_admin_bar->remove_menu('blog-' . $blog->userblog_id . '-c');
 			}
 		}
 		else {
 			// We have no way to know whether the plugin is active on other sites, so only remove this one
-			$wp_admin_bar->remove_menu( 'blog-' . get_current_blog_id() . '-c' );
+			$wp_admin_bar->remove_menu('blog-' . get_current_blog_id() . '-c');
 		}
 	}
 
@@ -171,29 +171,29 @@ class DisableAnyComment {
 	 * Return context-aware tools page URL
 	 */
 	private function tools_page_url() {
-		$base =  $this->networkactive ? network_admin_url( 'settings.php' ) : admin_url( 'tools.php' );
-		return add_query_arg( 'page', 'disable_any_comment_tools', $base );
+		$base =  $this->networkactive ? network_admin_url('settings.php') : admin_url('tools.php');
+		return add_query_arg('page', 'disable_any_comment_tools', $base);
 	}
 
 	public function filter_admin_menu(){
 		global $pagenow;
 
-		if ( $pagenow == 'comment.php' || $pagenow == 'edit-comments.php' )
-			wp_die( __( 'Comments are closed.' ), '', array( 'response' => 403 ) );
+		if ($pagenow == 'comment.php' || $pagenow == 'edit-comments.php')
+			wp_die(__('Comments are closed.'), '', ['response' => 403]);
 
-		remove_menu_page( 'edit-comments.php' );
+		remove_menu_page('edit-comments.php');
 
-		if ( $pagenow == 'options-discussion.php' )
-			wp_die( __( 'Comments are closed.' ), '', array( 'response' => 403 ) );
+		if ($pagenow == 'options-discussion.php')
+			wp_die(__('Comments are closed.'), '', ['response' => 403]);
 
-		remove_submenu_page( 'options-general.php', 'options-discussion.php' );
+		remove_submenu_page('options-general.php', 'options-discussion.php');
 	}
 
 	public function filter_dashboard(){
-		remove_meta_box( 'dashboard_recent_comments', 'dashboard', 'normal' );
+		remove_meta_box('dashboard_recent_comments', 'dashboard', 'normal');
 	}
 
-	public function admin_css(){
+	public function admin_css() {
 		echo '<style>
 			#dashboard_right_now .comment-count,
 			#dashboard_right_now .comment-mod-count,
@@ -209,36 +209,35 @@ class DisableAnyComment {
 		return [];
 	}
 
-	public function filter_comment_status( $open, $post_id ) {
+	public function filter_comment_status($open, $post_id) {
 		return false;
 	}
 
 	/**
 	 * Add links to Settings page
 	*/
-	public function plugin_actions_links( $links, $file ) {
+	public function plugin_actions_links($links, $file) {
 		static $plugin;
-		$plugin = plugin_basename( __FILE__ );
-		if( $file == $plugin && current_user_can('manage_options') ) {
+		$plugin = plugin_basename(__FILE__);
+		if($file == $plugin && current_user_can('manage_options')) {
 			array_unshift(
 				$links,
-				sprintf( '<a href="%s"><i style="font: 16px dashicons; vertical-align: text-bottom;" class="dashicon dashicons-admin-tools"></i></a>', esc_attr( $this->tools_page_url()))
+				sprintf('<a href="%s"><i style="font: 16px dashicons; vertical-align: text-bottom;" class="dashicon dashicons-admin-tools"></i></a>', esc_attr($this->tools_page_url()))
 			);
 		}
-
 		return $links;
 	}
 
 	public function tools_menu() {
-		$title = __( 'Delete Comments', 'disable-comments' );
-		if( $this->networkactive )
-			add_submenu_page( 'settings.php', $title, $title, 'manage_network_plugins', 'disable_any_comment_tools', array( $this, 'tools_page' ) );
+		$title = __('Delete Comments', 'disable-comments');
+		if($this->networkactive)
+			add_submenu_page('settings.php', $title, $title, 'manage_network_plugins', 'disable_any_comment_tools', [ $this, 'tools_page' ]);
 		else
-			add_submenu_page( 'tools.php', $title, $title, 'manage_options', 'disable_any_comment_tools', array( $this, 'tools_page' ) );
+			add_submenu_page('tools.php', $title, $title, 'manage_options', 'disable_any_comment_tools', [ $this, 'tools_page' ]);
 	}
 
 	public function tools_page() {
-		include dirname( __FILE__ ) . '/includes/tools-page.php';
+		include dirname(__FILE__) . '/includes/tools-page.php';
 	}
 
 }
--- includes/tools-page.php
+++ includes/tools-page.php
@@ -2,7 +2,7 @@
 
 namespace XXSimoXX\DisableAnyComment;
 
-if( !defined( 'ABSPATH' ) ) {
+if(!defined('ABSPATH')) {
 	exit;
 }
 
@@ -11,42 +11,42 @@ echo '<h1>' . __( 'Delete Comments', 'disable-comments') . '</h1>';
 
 global $wpdb;
 $comments_count = $wpdb->get_var("SELECT count(comment_id) from $wpdb->comments");
-if ( $comments_count <= 0 ) {
-	echo '<p><strong>' . __( 'No comments available for deletion.', 'disable-comments') . '</strong></p></div>';
+if ($comments_count <= 0) {
+	echo '<p><strong>' . __('No comments available for deletion.', 'disable-comments') . '</strong></p></div>';
 	return;
 }
 
-$typeargs = array( 'public' => true );
-if( $this->networkactive ) {
+$typeargs = ['public' => true];
+if ($this->networkactive) {
 	$typeargs['_builtin'] = true;	// stick to known types for network
 }
-$types = get_post_types( $typeargs, 'objects' );
-foreach( array_keys( $types ) as $type ) {
-	if( ! in_array( $type, $this->modified_types ) && ! post_type_supports( $type, 'comments' ) )	// the type doesn't support comments anyway
-		unset( $types[$type] );
+$types = get_post_types($typeargs, 'objects');
+foreach (array_keys($types) as $type) {
+	if(!in_array($type, $this->modified_types) && !post_type_supports($type, 'comments'))	// the type doesn't support comments anyway
+		unset($types[$type]);
 }
 
-if ( isset( $_POST['delete'] ) && isset( $_POST['delete_mode'] ) ) {
+if (isset($_POST['delete']) && isset($_POST['delete_mode'])) {
 	check_admin_referer( 'delete-comments-admin' );
 
-	if( $_POST['delete_mode'] == 'delete_everywhere' ) {
-		if ( $wpdb->query( "TRUNCATE $wpdb->commentmeta" ) != FALSE ){
-            if ( $wpdb->query( "TRUNCATE $wpdb->comments" ) != FALSE ){
-                $wpdb->query( "UPDATE $wpdb->posts SET comment_count = 0 WHERE post_author != 0" );
-                $wpdb->query( "OPTIMIZE TABLE $wpdb->commentmeta" );
-                $wpdb->query( "OPTIMIZE TABLE $wpdb->comments" );
-            	echo "<p style='color:green'><strong>" . __( 'All comments have been deleted.', 'disable-comments' ) . "</strong></p>";
+	if($_POST['delete_mode'] == 'delete_everywhere') {
+		if ( $wpdb->query("TRUNCATE $wpdb->commentmeta") != FALSE ){
+            if ( $wpdb->query("TRUNCATE $wpdb->comments") != FALSE ){
+                $wpdb->query("UPDATE $wpdb->posts SET comment_count = 0 WHERE post_author != 0");
+                $wpdb->query("OPTIMIZE TABLE $wpdb->commentmeta");
+                $wpdb->query("OPTIMIZE TABLE $wpdb->comments");
+            	echo "<p style='color:green'><strong>" . __('All comments have been deleted.', 'disable-comments') . "</strong></p>";
             } else {
-	            echo "<p style='color:red'><strong>" . __( 'Internal error occured. Please try again later.', 'disable-comments' ) . "</strong></p>";
+	            echo "<p style='color:red'><strong>" . __('Internal error occured. Please try again later.', 'disable-comments') . "</strong></p>";
             }
         } else {
-            echo "<p style='color:red'><strong>" . __( 'Internal error occured. Please try again later.', 'disable-comments' ) . "</strong></p>";
+            echo "<p style='color:red'><strong>" . __('Internal error occured. Please try again later.', 'disable-comments') . "</strong></p>";
         }
 	}
 
 	$comments_count = $wpdb->get_var("SELECT count(comment_id) from $wpdb->comments");
 	if ( $comments_count <= 0 ) { 
-		echo '<p><strong>' . __( 'No comments available for deletion.', 'disable-comments') . '</strong></p></div>';
+		echo '<p><strong>' . __('No comments available for deletion.', 'disable-comments') . '</strong></p></div>';
 		return;
 	}
 
@@ -54,6 +54,6 @@ if ( isset( $_POST['delete'] ) && isset( $_POST['delete_mode'] ) ) {
 
 echo '<form action="" method="post" id="delete-comments">';
 echo '<input type="hidden" id="delete_everywhere" name="delete_mode" value="delete_everywhere" />';
-wp_nonce_field( 'delete-comments-admin' );
-echo '<h4>' . __( 'Total Comments: ', 'disable-comments' ) . $comments_count . '</h4>';
-echo '<p class="submit"><input class="button-primary" type="submit" name="delete" value="' .  __( 'Delete Comments', 'disable-comments') . '"></p></form></div>';
+wp_nonce_field('delete-comments-admin');
+echo '<h4>' . __('Total Comments: ', 'disable-comments') . $comments_count . '</h4>';
+echo '<p class="submit"><input class="button-primary" type="submit" name="delete" value="' .  __('Delete Comments', 'disable-comments') . '"></p></form></div>';
